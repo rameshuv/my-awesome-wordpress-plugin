633aeae875c14684902387dadd09c53934df672f
rameshuv <get.ramesh.uv@gmail.com>
2025-09-06 21:10:22 +0530

    feat: add user search, sorting and pagination

--- admin/views/users.php
+++ admin/views/users.php
@@ -1,5 +1,4 @@
 <?php
-// phpcs:ignoreFile -- Legacy view requires refactoring for WordPress coding standards.
 /**
  * Users admin page.
  *
@@ -7,24 +6,24 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+		exit;
 }
 
 if ( ! current_user_can( 'manage_options' ) ) {
-	wp_die( esc_html__( 'You do not have sufficient permissions to access this page.', 'bonus-hunt-guesser' ) );
+		wp_die( esc_html__( 'You do not have sufficient permissions to access this page.', 'bonus-hunt-guesser' ) );
 }
 
 if ( ! class_exists( 'BHG_Users_Table' ) ) {
-	require_once BHG_PLUGIN_DIR . 'admin/class-bhg-users-table.php';
+		require_once BHG_PLUGIN_DIR . 'admin/class-bhg-users-table.php';
 }
 
 $table = new BHG_Users_Table();
 $table->prepare_items();
 ?>
 <div class="wrap">
-	<h1 class="wp-heading-inline"><?php echo esc_html__( 'Users', 'bonus-hunt-guesser' ); ?></h1>
-	<form method="get">
-		<input type="hidden" name="page" value="bhg-users" />
-		<?php $table->display(); ?>
-	</form>
+		<h1 class="wp-heading-inline"><?php echo esc_html__( 'Users', 'bonus-hunt-guesser' ); ?></h1>
+		<form method="get">
+				<input type="hidden" name="page" value="<?php echo esc_attr( 'bhg-users' ); ?>" />
+				<?php $table->display(); ?>
+		</form>
 </div>
